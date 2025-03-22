0842d8ca9a51b81393365584d814e28da0b3734f
mohsenh1989 <mohsen.heydarpour@gmail.com>
2025-03-22 13:56:05 +0330

    Initial commit

---
+++ ExcelMetohd.py
@@ -0,0 +1,24 @@
+from openpyxl import load_workbook
+from xlwt import Workbook
+
+
+# Press the green button in the gutter to run the script.
+# wb = load_workbook("Test.xlsx")
+#
+# sh = wb['scores']
+#
+# print(sh['A2'].value)
+# print(sh['B4'].value)
+# print(sh.cell(row=3, column=1))
+
+wb = Workbook()
+sheet1 = wb.add_sheet('New')
+
+
+
+
+for i in range(0,7):
+    sheet1.write(1, i, 'k')
+
+wb.save('Exam.xls')
+
---
+++ OsMethod.py
@@ -0,0 +1,12 @@
+import os
+
+
+
+r=os.getcwd()
+d=os.chdir("D:\Files")
+
+l = os.listdir()
+for i in l:
+    print(i)
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
